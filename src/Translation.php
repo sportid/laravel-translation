f8ad4d9f774513bfba8041d33b97543d9184f8fa
Sander Sats <sander.sats@gmail.com>
2021-02-23 17:14:59 +0200

    Replace translation update with add to enable overwriting empty translations

--- src/Translation.php
+++ src/Translation.php
@@ -194,7 +194,7 @@ class Translation
                     ->first()
                     ->toJson();
 
-                $this->query('https://api.poeditor.com/v2/translations/update', [
+                $this->query('https://api.poeditor.com/v2/translations/add', [
                     'form_params' => [
                         'api_token' => $this->apiKey,
                         'id' => $this->projectId,
